7550978f01b30a2057e0585872689108c00faa08
muhammed akyuzlu <muhammedakyuzlu6@gmail.com>
2021-02-07 18:12:53 +0300

    last change

--- lib/ui/drive/moneyBox.dart
+++ lib/ui/drive/moneyBox.dart
@@ -9,7 +9,7 @@ class MoneyBox extends StatefulWidget {
 class _MoneyBoxState extends State<MoneyBox> {
   @override
   Widget build(BuildContext context) {
-     Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -23,13 +23,53 @@ class _MoneyBoxState extends State<MoneyBox> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              width: size.width*0.5,
-
-              child: SvgPicture.asset('assets/images/kumbaram.svg'),
+              width: size.width,
+              child: Image.asset('assets/images/kumbaram.png'),
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  'vaervaervaervjkarbvbrivbaebrv;abure;vba;eruvb',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: Container(
+                child: GestureDetector(
+                  onTap: donat,
+                  child: Container(
+                    width: size.width * 0.6,
+                    height: size.height * 0.06,
+                    margin: EdgeInsets.only(
+                        left: size.width * 0.15, right: size.width * 0.15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Color(0xFF193566),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Bağış",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
+  donat() {}
 }
